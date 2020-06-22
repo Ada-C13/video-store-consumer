efc528d9e9091f1e232f081b14640ea8f867b6a7
dnguye2 <dnguyen1093@gmail.com>
2020-06-22 16:03:09 -0700

    routes generated for search, customers, library, and home
    Co-authored-by: Leah <leahwhughes@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,54 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
+      <Router>
+        <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/search">Search</Link>
+            </li>
+            <li>
+              <Link to="/library">Library</Link>
+            </li>
+            <li>
+              <Link to="/customers">Customers</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/search">
+            <Search />
+          </Route>
+          <Route path="/customers">
+            <Customers />
+          </Route>
+          <Route path="/library">
+            <Library />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+
+      
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -14,8 +58,24 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
+      </Router>
     );
   }
 }
 
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function Search() {
+  return <h2>Search</h2>;
+}
+
+function Customers() {
+  return <h2>Customers</h2>;
+}
+
+function Library() {
+  return <h2>Library</h2>;
+}
 export default App;
