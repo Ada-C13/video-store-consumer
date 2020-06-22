7f0edb3883141e7a21574aec8518bf44b2da0d1f
dnguye2 <dnguyen1093@gmail.com>
2020-06-22 16:42:02 -0700

    created search component

---
+++ src/components/Search.js
@@ -0,0 +1,8 @@
+import React, {useState} from 'react';
+
+const Search = () => {
+  return <div>WHAT ARE YOU SEARCHING FOR??</div>
+  // Here we will also return some movie cards and that will be great
+};
+
+export default Search;
