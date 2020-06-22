48956830b437da9ebe3f73f25dd900b3fc01ca06
Nataliya Pogodina <n.pogodina@gmail.com>
2020-06-22 16:17:03 -0700

    Added onInputChange to search"

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SearchBar from './SearchBar';
 
 const Search = () => {
-  
+  const [movies, setMovies] = useState([]);
+
+  const searchMovies = () => {
+
+  };
 
   return (
-    <div className="">
-      <SearchBar />
+    <div className="ui container">
+      <div className="">
+        <SearchBar searchMovies={searchMovies} />
+      </div>
+        <div class="ui horizontal divider">
+        Results
+      </div>
     </div>
   )
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const SearchBar = () => {
+const SearchBar = (props) => {
+
+  const [searchInput, setSearchInput] = useState("");
+  
+  const onInputChange = (event) => {
+    const newInput = event.target.value;
+    setSearchInput(newInput);
+  };
+
+  const onFormSubmit = (event) => {
+
+  };
   
   return (
     <div className="ui container">
-      <h1 class="ui header">Type the movie name:</h1>
-      <div className="ui big search">
-        <input className="prompt" type="text" placeholder="Frozen" />
-      </div>
+      <h1 className="ui header">Type the movie name:</h1>
+      <form className="ui input big">
+        <input type="text" placeholder="Frozen" onChange={onInputChange} />
+        <button className="ui big button">Search</button>
+      </form>
     </div>
   );
 }
