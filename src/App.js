2fe947b67f3ffb63f303b9d7ca30c46bdca9e750
Haben <haben7@gmail.com>
2020-06-22 19:17:39 -0700

    makes use of the refresher component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import AppHeader from './components/AppHeader'
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => {
+  const [counter, setCounter] = useState(0);
+  const incrementCounter = () => {
+    setCounter(counter + 1);
+  };
+  
+  return (
+    <div className="App">
+      <AppHeader 
+        text="Click Refresher in React" 
+        counter={counter}
+        clickCallback={incrementCounter} 
+        />
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+    </div>
+  );
+};
+ 
 
 export default App;
