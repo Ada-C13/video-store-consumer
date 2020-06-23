14ad17708246abcde8b3d534c4041e8cb7562d03
Tithvorlak Mok <tithvorlakmok@gmail.com>
2020-06-23 15:48:55 -0700

    Added import bootrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
 import React from 'react';
 import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const App = () => {
     return (
-      <Header />
+      <section>
+        <Header />
+      </section>
     );
 }
 
