63762dd70477b12f56816d77f70025d53c0fd2bc
Nataliya Pogodina <n.pogodina@gmail.com>
2020-06-23 09:24:21 -0700

    Added black navbar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div className="">
-      Welcome home!
+    <div class="ui text container">
+      <h1 class="">
+        Imagine-a-Company
+      </h1>
+      <h2>Do whatever you want when you want to.</h2>
+      <div class="ui huge primary button">Get Started <i class="right arrow icon"></i></div>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="">
-      I am a navbar!
+    <div class="ui inverted segment">
+      <div class="ui inverted secondary pointing menu">
+        <a class="item">
+          <Link to={`/home`} className="">Home</Link>
+        </a>
+        <a class="active item">
+          <Link to={`/search`} className="">Search</Link>
+        </a>
+        <a class="item">
+          Library
+        </a>
+        <a class="item">
+          Customers
+        </a>
+      </div>
     </div>
   )
 }
