3fc1ec78920e25075adbefc21cfffdccb2222bf4
dnguye2 <dnguyen1093@gmail.com>
2020-06-23 13:18:52 -0700

    got axios working and Rails connected
    Co-authored-by: Leah <leahwhughes@gmail.com>

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,33 @@
 import React, {useState} from 'react';
+import axios from "axios";
+
+URL = "http://localhost:5000/movies"
 
 const Search = () => {
-  return <div>WHAT ARE YOU SEARCHING FOR??</div>
+
+  const [query, setQuery] = useState('');
+  const [movie, setMovie] = useState('');
+
+  const search = (event) => {
+    event.preventDefault();
+
+    axios.get('http://localhost:5000/movies')
+    .then((response) => {
+      console.log(`YO HERE IS THE RESPONSE: ${response}`);
+    })
+    .catch((error) => {
+      console.log(`THIS GOT THROUGH SO HERE IS THE ERROR: ${error.response}`);
+    });
+  };
+
+  return (
+  <form className='form' onSubmit={search}>
+    <label className='label' htmlFor="query">Movie Name</label>
+    <input className='input' type='text' name='query' placeholder='Enter a Movie' value={query} onChange={ (event) => setQuery(event.target.value) }/>
+    <button className='button' type='submit'>Search </button>
+  </form>
+  // render some movie cards
+  );
   // Here we will also return some movie cards and that will be great
 };
 
