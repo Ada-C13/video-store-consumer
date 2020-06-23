7c83c8a9409dbc26f7f25467445b7f2deeee4828
Jocelyn Wang <wangjoc@gmail.com>
2020-06-23 07:03:28 -0700

    added in movie library to create movie list

---
+++ src/components/MovieLib.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Movie from './Movie'
+import './MovieLib.css'
+
+const MovieLib = ({ movieList, selectMovie, detailsCallback, detailsMovie }) => {
+  
+  const makeMovies = () => movieList.map((movie, i) => {
+    return <Movie
+      key={i}
+      { ...movie }
+      selectMovie={() => selectMovie(movie.external_id)}
+      inLibrary="true"
+      detailsCallback={() => detailsCallback(movie.external_id) }
+      detailsMovie={detailsMovie}
+      />
+  });
+  
+  return (
+    <div>
+      <h3>Movie Library</h3>
+      <div className="row">
+        {makeMovies()}
+      </div>
+    </div>
+  )
+}
+
+MovieLib.propTypes = {
+  movieList: PropTypes.array.isRequired,
+  selectMovie: PropTypes.func.isRequired,
+  detailsCallback: PropTypes.func.isRequired,
+  detailsMovie: PropTypes.object,
+}
+
+export default MovieLib;
