7da028a224f358480ab16ca0183a2ca57b6693eb
Nataliya Pogodina <n.pogodina@gmail.com>
2020-06-22 21:40:24 -0700

    Fixed images for found movies

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 const SearchResult = (props) => {
+
+
   
   return (
     <tr>
-      <td class="ui image header">
-        <img src="/images/avatar2/small/lena.png" className="ui mini rounded image"/>
+      <td>
+        <img src={props.image_url} className="ui mini rounded image"/>
       </td>
       <td> {props.title} </td>
       <td> {props.release_date} </td>
