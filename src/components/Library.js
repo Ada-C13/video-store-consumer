dbb935fd1405b5ba0bca41d3632d82c07eac8ec5
dnguye2 <dnguyen1093@gmail.com>
2020-06-23 16:03:38 -0700

    library collection of movies view working
    Co-authored-by: Leah <leahwhughes@gmail.com>

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,7 +1,35 @@
 import React, {useState} from 'react';
+import axios from "axios";
+import MovieCard from './MovieCard';
 
 const Library = () => {
-  return <div>THIS IS THE LIBRARY, BITCHEZ.  WHERE IS YOUR LIBRARY CARD?</div>
+  const [error, setError] = useState(null);
+  const [libraryList, setLibraryList] = useState(null);
+
+  axios.get('http://localhost:3000/movies')
+    .then((response) => {
+      const data = response.data;
+      console.log(`this is response.data ${response.data}`);
+      let movieCollection = data.map((movie) => {
+        return (
+          <MovieCard
+          external_id = {movie.external_id}
+          title = {movie.title}
+          overview = {movie.overview}
+          release_date = {movie.release_date}
+          image_url = {movie.image_url}
+          />
+        );
+      });
+      setLibraryList(movieCollection);
+    })
+    .catch((error) => {
+      setError(error.response);
+    });
+
+    return (
+      <div>{libraryList}</div>
+    );
 };
 
 
