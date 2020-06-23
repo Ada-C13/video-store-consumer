edf1d7109cf4dabcd9da30102db1a9d99bb15de5
Jetmire Bajrami <jetmirebajrami@Jetmires-MacBook-Pro.local>
2020-06-23 00:58:57 -0700

    Add Rails Route sceletom in App Component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Link, Switch} from "react-router-dom";
+import CustomerList from './components/CustomerList'
+import MovieLib from './components/MovieLib'
+import MovieSearch from './components/MovieSearch'
+import Home from './components/Home'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <Router>
+        <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <ul>
+            <li>
+              <Link path="/">Home</Link>
+            </li>
+            <li>
+              <Link path="/search">Search for Movie</Link>
+            </li>
+            <li>
+              <Link path="/library">Library</Link>
+            </li>
+            <li>
+              <Link path="/customers">Customer List</Link>
+            </li>
+          </ul>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+     
+        <Switch>
+        <Route path="/" component={Home}/>
+        <Route path="/search" component={MovieSearch}/>
+        <Route path="/library" component={MovieLib}/>
+        <Route path="/customers" component={CustomerList} />
+        </Switch>
+        </div>
+      </Router>
     );
   }
 }
 
 export default App;
+
+
