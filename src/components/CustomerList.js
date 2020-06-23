492f3df9e94829fde70bdf952f01e49e33360062
Jetmire Bajrami <jetmirebajrami@Jetmires-MacBook-Pro.local>
2020-06-23 00:50:31 -0700

    Added ListComponent file and added component skeleton

---
+++ src/components/CustomerList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './CustomerList.css';
+
+
+class CustomerList extends Component {
+  render () {
+    return (
+      <div>
+        <h1>Costumer List</h1>
+      </div>
+    );
+  }
+};
+export default CustomerList;
