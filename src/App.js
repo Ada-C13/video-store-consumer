d52cc3c753a11b86b17d9ec82a5409c17bf56b55
denisseai <msdenisseanaya@gmail.com>
2020-06-23 11:01:05 -0700

    Added import for router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,54 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './components/Home';
+import CustomerList from './components/CustomerList';
+import MovieSearch from './components/MovieSearch';
+import MovieLibrary from './components/MovieLibrary'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/customers">Customers</Link>
+            </li>
+            <li>
+              <Link to="/library">Movie Library</Link>
+            </li>
+            <li>
+              <Link to="/search">Movie Search</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/customers">
+            <CustomerList />
+          </Route>
+          <Route path="/library">
+            <MovieLibrary />
+          </Route>
+          <Route path="/search">
+            <MovieSearch />
+          </Route>
+        </Switch>
       </div>
+    </Router>
     );
   }
 }
