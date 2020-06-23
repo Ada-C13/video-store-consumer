3cf66e350999354c0bd97e20314add24cc4154c7
Jetmire Bajrami <jetmirebajrami@Jetmires-MacBook-Pro.local>
2020-06-23 00:52:54 -0700

    Added MovieLib file js/css and added component skeleton

---
+++ src/components/MovieLib.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './MovieLib.css';
+
+
+class MovieLib extends Component {
+  render () {
+    return (
+      <div>
+        <h1>Movie Library</h1>
+      </div>
+    );
+  }
+};
+export default MovieLib;
