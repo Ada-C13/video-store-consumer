d471bd79a952cb42d929d75d2ad73fc5f19f37fe
Vera <lizeth.veraro@outlook.com>
2020-06-22 17:51:08 -0700

    Seach bar working

--- src/components/Store.js
+++ src/components/Store.js
@@ -4,74 +4,52 @@ import axios from 'axios';
 import Movie from './Movie';
 import Search from './Search';
 
-const reformatData = (data) => {
-  return data.map((element) => {
-    return element;
-  });
-};
 
 const Store = (props) => {
+
   const [movie, setMovie] = useState([]);
-  const onSubmitCallback = (movie) => {
-    axios.get(`${props.url}movies/${movie}`) 
-    .then ((response) => {
-    setMovie(response)
-    
-    })
-
-    .catch((error) => {
-      console.log(`Error: ${error}`)
-    });
-  }
-
-  // The useState function returns an array which contains 
-  // two items: movieList and SetMovieList.
-  // The movieList is the variable which will store our value and SetMovieList
-  // is an updater function which will be responsible to update the movieList.
-  const [movieList, setMovieList] = useState([]);
   const endPoint = `${props.url}movies`
-  console.log(endPoint)
 
-  const getMovies = (url) => {
-
-    axios.get(url)
+  const onSubmitCallback = (movie) => {
+    axios.get(`${props.url}movies/${movie}`)
       .then((response) => {
         console.log(response.data)
-        const apiMovie = reformatData(response.data);
-        setMovieList(apiMovie);
+        setMovie(response.data)
+
       })
       .catch((error) => {
-        console.log(error)
+        console.log(`Error: ${error}`)
       });
-  };
+  }
 
   // By using this Hook, you tell React that your component needs to do something 
   // after render.
   // useEffect takes a function(getMovies) which can contain any kind of operation including side effects
   // Also takes a second argument as an array [], 
   // in this array you can pass variables. When any of this variable updates it will cause the useEffect to run again
-  useEffect(() => { getMovies(endPoint); }, [endPoint]);
+  useEffect(() => { onSubmitCallback(endPoint); }, [endPoint, movie]);
 
-  const formatMovies = movie.map((movie) => {
+  const formatMovies = (() => {
     return (
       <Movie
         key={movie.id}
         id={movie.id}
         title={movie.title}
         overview={movie.overview}
-        image_url={movie.image_url}
         release_date={movie.release_date}
+        available_inventory={movie.available_inventory}
+        inventory={movie.inventory}
       />
     );
   });
 
   return (
     <main>
-      <div><Search 
-        onSubmitCallback={onSubmitCallback}/>
+      <div><Search
+        onSubmitCallback={onSubmitCallback} />
       </div>
       <div className="store">
-        {formatMovies}
+        {formatMovies(movie)}
       </div>
     </main>
   );
