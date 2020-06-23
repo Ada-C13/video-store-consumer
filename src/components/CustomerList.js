b2efcc7d97dce4eeb1263e7c76e46fedb5fd8770
Jetmire Bajrami <jetmirebajrami@Jetmires-MacBook-Pro.local>
2020-06-23 13:42:06 -0700

    added useefect to get data form API for all movies

--- src/components/CustomerList.js
+++ src/components/CustomerList.js
@@ -1,14 +1,50 @@
-import React, { Component } from 'react';
+import React, { useState,  useEffect } from 'react';
 import './CustomerList.css';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+import Customer from './Customer'
+
+const CustomerList = (props) => {
+  
+  const API_CUSTOMER_URL = `${props.url}/customers`
+  const [customerList, setCustomerList] = useState([])
+  const [message, setMessage] = useState(null);
+
+  useEffect(()=>{
+    axios.get(API_CUSTOMER_URL)
+      .then((response) => {
+        const customerList = response.data;
+        setCustomerList(customerList);
+      })
+      .catch((error) => {
+        setMessage(error.message);
+        console.log(error.message);
+      });
+  }, [API_CUSTOMER_URL])
+
+  const customerComponents = customerList.map((customer) => {
+    return(
+      <Customer
+        key = {customer.id}
+        id = {customer.id}
+        id = {customer.name}
+        address = {customer.address}
+        city = {customer.city}
+        state = {customer.state}
+        postal_code = {customer.postal_code}
+        phone = {customer.phone}
+        registered_at = {customer.registered_at}
+        movies_checked_out_count = {customer.movies_checked_out_count}
+        account_credit = {customer.account_credit}
+      />
+    )
+  })
 
 
-class CustomerList extends Component {
-  render () {
-    return (
+  return (
       <div>
-        <h1>Costumer List</h1>
+        {customerComponents}
       </div>
-    );
-  }
+  );
 };
 export default CustomerList;
