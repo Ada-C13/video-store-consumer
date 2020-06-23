cdce592ab0a85a049507b0b7db78b37881d859a9
Jetmire Bajrami <jetmirebajrami@Jetmires-MacBook-Pro.local>
2020-06-23 13:38:42 -0700

    added react routes

--- src/App.js
+++ src/App.js
@@ -1,43 +1,54 @@
 import React, { Component } from 'react';
 import './App.css';
 import {BrowserRouter as Router, Route, Link, Switch} from "react-router-dom";
+// import PropTypes from 'prop-types';
+import axios from 'axios';
 import CustomerList from './components/CustomerList'
 import MovieLib from './components/MovieLib'
 import MovieSearch from './components/MovieSearch'
 import Home from './components/Home'
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div className="App">
-        <header className="App-header">
-          <ul>
-            <li>
-              <Link path="/">Home</Link>
-            </li>
-            <li>
-              <Link path="/search">Search for Movie</Link>
-            </li>
-            <li>
-              <Link path="/library">Library</Link>
-            </li>
-            <li>
-              <Link path="/customers">Customer List</Link>
-            </li>
-          </ul>
-        </header>
-     
-        <Switch>
-        <Route path="/" component={Home}/>
-        <Route path="/search" component={MovieSearch}/>
-        <Route path="/library" component={MovieLib}/>
-        <Route path="/customers" component={CustomerList} />
-        </Switch>
-        </div>
-      </Router>
-    );
-  }
+
+const App = () => {
+  
+
+  return (
+    <Router>
+      <div className="App">
+      <header className="App-header">
+        <ul>
+          <li>
+            <Link to="/"> Home</Link>
+          </li>
+          <li>
+            <Link to="/search">Search for Movie</Link>
+          </li>
+          <li>
+            <Link to="/library">Library</Link>
+          </li>
+          <li>
+            <Link to="/customers">Customer List</Link>
+          </li>
+        </ul>
+      </header>
+    
+      <Switch>
+      <Route exact path="/">
+        <Home/>
+      </Route>
+      <Route path="/search">
+        <MovieSearch/>
+      </Route> 
+      <Route path="/library">
+        <MovieLib url="http://localhost:3000/"/>
+      </Route>
+      <Route path="/customers">
+        <CustomerList url="http://localhost:3000/"/>
+      </Route> 
+      </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
