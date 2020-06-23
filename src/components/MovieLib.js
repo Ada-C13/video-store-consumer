cea501b5b52483b519a36e73d7cfe15123052211
Jetmire Bajrami <jetmirebajrami@Jetmires-MacBook-Pro.local>
2020-06-23 16:36:49 -0700

    Fixed the list with all the movies  component

--- src/components/MovieLib.js
+++ src/components/MovieLib.js
@@ -1,48 +1,25 @@
-import React, { useState,  useEffect } from 'react';
 import './MovieLib.css';
 import PropTypes from 'prop-types';
-import axios from 'axios';
 import Movie from './Movie'
 
-
-const MovieLib = (props) => {
-  const API_MOVIE_URL = `${props.url}/movies`
-  const [movieList, setMovieList] = useState([])
-  const [message, setMessage] = useState(null);
-
-  useEffect(()=>{
-    axios.get(API_MOVIE_URL)
-      .then((response) => {
-        const movieList = response.data;
-        setMovieList(movieList);
-      })
-      .catch((error) => {
-        setMessage(error.message);
-        console.log(error.message);
-      });
-  }, [API_MOVIE_URL])
- 
-      
-  const movieComponents = movieList.map((movie) => {
-    return(
-      <Movie
-        key = {movie.id}
-        id = {movie.id}
-        title = {movie.title}
-        overview = {movie.overview}
-        release_date = {movie.release_date}
-        image_url = {movie.image_url}
-        external_id = {movie.external_id}
+const MovieLib = ({ movieList, selectMovie }) => {
+  const buildMovie = () => {
+    const movie = movieList.map((movie) => {
+      return <Customer 
+        key={movie.id}
+        { ...movie }
+        selectMovie={(id) => selectMovie(id)}
       />
-    )
-  })
+    });
+    return movie;
+  }
+
   return (
     <div>
-      {movieComponents}
+      <h1>INFORMATION ABOUT MOVIES</h1>
+      {buildMovie()}
     </div>
-  );
-
- 
+  )
 }
 
 export default MovieLib;
