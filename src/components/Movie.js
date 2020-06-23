d1ea0ceba28d427381158f5a627e3cb30eca0a76
Jetmire Bajrami <jetmirebajrami@Jetmires-MacBook-Pro.local>
2020-06-23 16:47:54 -0700

    Fixed movie Component to render all the information for a movie.

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React , { Component}  from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
+import { Button } from 'react-bootstrap/';
 
+const Movie = ({ id, title, overview, release_date, image_url, external_id, selectMovie }) => {
+  return (
+  <div className="">
+    <div className="">
+      <h3> {title} </h3>
+      <ul>
+        <li>Overview: {overview} </li>
+        <li>Relase Date: {(release_date).substring(0,10)} </li>
+        <li>Movies Checked Out: {release_date} </li>
+        <li>Photo: {image_url} </li>
+        <li>External id: {external_id} </li>
+      </ul>
+    </div>
+    <div className="">
+      <Button
+        className=""
+        onClick={() => selectMovie(id)}>
+          Add Movie
+      </Button> 
+    </div>  
+  </div>
+  )
+};
 
-const Movie = (props) => {
-  
-  return(
-  <div> 
-    {props.id}
-    {props.title}
-    {props.overview}
-    {props.release_date}
-    {props.image_url}
-    {props.external_id}
-  </div>)
-}
-
-export default Movie
+export default Movie;
