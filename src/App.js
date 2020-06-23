17ae0293398613d5c0cc48ef7632e010f0b0993b
Alicia Combs <alicia.combs.92@gmail.com>
2020-06-23 10:27:59 -0700

    Added home, library, customers, and search components and nav

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import Customers from './components/Customers';
+import Library from './components/Library';
+import Search from './components/Search';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+    <Router>
+        <div>
+          <h2>Welcome to React Router Tutorial</h2>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <ul className="navbar-nav mr-auto">
+            <li><Link to={'/'} className="nav-link"> Home </Link></li>
+            <li><Link to={'/library'} className="nav-link">Library</Link></li>
+            <li><Link to={'/customers'} className="nav-link">Customers</Link></li>
+            <li><Link to={'/search'} className="nav-link">Search</Link></li>
+          </ul>
+          </nav>
+          <hr />
+          <Switch>
+              <Route exact path='/' component={Home} />
+              <Route path='/library' component={Library} />
+              <Route path='/customers' component={Customers} />
+              <Route path='/search' component={Search} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/Library.js
@@ -0,0 +1,15 @@
+// Home.js
+
+import React, { Component } from 'react';
+
+class Library extends Component {
+  render() {
+    return (
+        <div>
+          <h2>Library</h2>
+        </div>
+    );
+  }
+}
+
+export default Library;
