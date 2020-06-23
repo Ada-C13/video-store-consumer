3723d49071b0f7302fe9b6df6b64c138df4a5613
dnguye2 <dnguyen1093@gmail.com>
2020-06-23 16:44:45 -0700

    search results can be displayed

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,42 +1,69 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from "axios";
+import MovieCard from './MovieCard';
 
 const Search = () => {
-
+  
   const [query, setQuery] = useState('');
   const [moviesList, setMoviesList] = useState([]);
   const [error, setError] = useState(null);
+
+  // useEffect(() => {
+  //   axios.get(API_URL_BASE)
+  //     .then((response) => {
+  //       const apiStudentList = response.data;
+  //       setStudentList(apiStudentList);
+  //     })
+  //     .catch((error) => {
+  //       setErrorMessage(error.message);
+  //       console.log(error.message);
+  //     });
+  // }, []);
 
   const search = (event) => {
     event.preventDefault();
 
     axios.get('http://localhost:3000/movies', {
-      params: {
-        query: query
-      }
-    })
-    .then((response) => {
-      setMoviesList(response.data);
+    params: {
+      query: query
+    }
+  })
+  .then((response) => {
+    const data = response.data;
+    console.log(`this is response.data ${response.data}`);
+    console.log(`this is movies list before: ${moviesList}`);
+    let movieCollection = data.map((movie) => {
+      return (
+        <MovieCard
+        external_id = {movie.external_id}
+        title = {movie.title}
+        overview = {movie.overview}
+        release_date = {movie.release_date}
+        image_url = {movie.image_url}
+        />
+        );
+      });
+      setMoviesList(movieCollection);  
     })
     .catch((error) => {
       setError(error.response);
     });
   };
-
-  // const movies = moviesList.map(movie =>
-  //   make <Movie /> components, add those to the return of the Search component
-  // );
-
+  
   return (
-  <form className='form' onSubmit={search}>
+  <div>
+    <form className='form' onClick={search}>
     <label className='label' htmlFor="query">Movie Name</label>
     <input className='input' type='text' name='query' placeholder='Enter a Movie' value={query} onChange={ (event) => setQuery(event.target.value) }/>
     <button className='button' type='submit'>Search </button>
-  </form>
-  // render some movie cards
-  // each card should also have a button to add it to the rental store database
-  );
-  // Here we will also return some movie cards and that will be great
-};
-
-export default Search;
+    </form>
+    <h1>{moviesList}</h1>   
+  </div>
+    // render some movie cards
+    // each card should also have a button to add it to the rental store database
+    );
+    // Here we will also return some movie cards and that will be great
+  };
+  
+  export default Search;
+  
