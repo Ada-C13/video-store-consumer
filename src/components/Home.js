2d530677e34c7bdbec085c9c1ecb894f18feb991
Jocelyn Wang <wangjoc@gmail.com>
2020-06-23 07:02:12 -0700

    added in home page

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+//import PropTypes from 'prop-types';
+import './Home.css'
+
+const Home = () => {
+  return (
+    <h1>Shonda and Jocelyn's Video Store!</h1>
+  )
+}
+
+export default Home;
