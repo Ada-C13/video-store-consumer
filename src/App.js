de6ae95dfa14a08148b270a2b529c1c75c37fd8b
Alicia Combs <alicia.combs.92@gmail.com>
2020-06-23 15:58:06 -0700

    Adding comments to app

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const App = () => {
+  //create a callback func for customers
+  //use effect statement to store selectedCustomer locally
+  //add func to clear it
+  //add func to set it to a new one
+
+
     return (
       <Header />
     );
