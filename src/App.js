41f88f210233f9913819ec2a9fd21ab305d035ce
SuelyBarreto <suely@barreto.us>
2020-06-22 18:49:52 -0700

    Created selected movie and selected customer states.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Link } from "react-router-dom";
 
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 
 import "./App.css";
 import Home from "./components/Home";
@@ -10,43 +11,77 @@ import Search from "./components/Search";
 import Library from "./components/Library";
 import Customers from "./components/Customers";
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div className="AppRoute">
-          {drawNav()}
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/search" component={Search} />
-            <Route path="/library" component={Library} />
-            <Route path="/customers" component={Customers} />
-          </Switch>
-        </div>
-      </Router>
-    );
-  }
-}
+// App component
+const App = () => {
+  console.log(`App, will initialize`);
+  // Declare and initialize state
+  const [selectedMovie, setMovie] = useState("");
+  const [selectedCustomer, setCustomer] = useState("");
 
-function drawNav() {
+  // Callback function to select movie
+  const onMovieSelectCallback = (strMovie) => {
+    console.log(`App, onMovieSelectCallback`, strMovie);
+    // change state
+    setMovie(strMovie);
+  };
+
+  // Callback function to select customer
+  const onCustomerSelectCallback = (strCustomer) => {
+    console.log(`App, onCustomerSelectCallback`, strCustomer);
+    // change state
+    setCustomer(strCustomer);
+  };
+
+  const drawNav = () => {
+    console.log(`App, draw navigation`);
+    return (
+      <nav>
+        <ul className="nav-links">
+          <Link to="/">
+            <li>Home</li>
+          </Link>
+          <Link to="/search">
+            <li>Search</li>
+          </Link>
+          <Link to="/library">
+            <li>Library</li>
+          </Link>
+          <Link to="/customers">
+            <li>Customers</li>
+          </Link>
+        </ul>
+      </nav>
+    );
+  };
+
+  const drawSelected = () => {
+    console.log(`App, draw selected`);
+    return (
+      <div>
+        <ul className="selected">
+          <li>Selected movie: {selectedMovie}</li>
+          <li>Selected customer: {selectedCustomer}</li>
+        </ul>
+      </div>
+    );
+  };
+
+  // render() {
   return (
-    <nav>
-      <ul className="nav-links">
-        <Link to="/">
-          <li>Home</li>
-        </Link>
-        <Link to="/search">
-          <li>Search</li>
-        </Link>
-        <Link to="/library">
-          <li>Library</li>
-        </Link>
-        <Link to="/customers">
-          <li>Customers</li>
-        </Link>
-      </ul>
-    </nav>
+    <Router>
+      <div className="AppRoute">
+        {drawNav()}
+        {drawSelected()}
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/search" component={Search} />
+          <Route path="/library" component={Library} />
+          <Route path="/customers" component={Customers} />
+        </Switch>
+      </div>
+    </Router>
   );
-}
+  // }
+};
 
 export default App;
