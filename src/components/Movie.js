dba315893483c22a0ba604f2dfcf3d87ed78bd17
Vera <lizeth.veraro@outlook.com>
2020-06-24 12:51:15 -0700

    callback function to select a movie

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,27 +2,23 @@ import React from 'react';
 import './Movie.css';
 
 const Movie = (props) => {
-  console.log(props)
+  const {movie} = props
 
   return (
     <div id={props.id} className="movie">
-      
       {
-        !props.title || (
+        movie && (
           <section>
             <div class="container">
               <div class="col-md-4">
                 <div class="card profile-card-1">
-                    <h5 class="card-title">Tilte: {props.title} </h5>
+                    <h5 class="card-title">Tilte: {movie.title} </h5>
                     <div class="card-content">
-                    <p className="movie__content-text">Overview: {props.overview} </p>
-                    <p className="movie__content-text">Release date: {props.release_date} </p>
-                    {/* <p className="movie__content-text">Total inventory: {props.inventory} </p> */}
-                    <button
-                      className="add-library-button"
-                      // onClick={props.onSelectMovie}
-                    > Select
-                    </button>
+                    <p className="movie__content-text">Overview: {movie.overview} </p>
+                    <p className="movie__content-text">Release date: {movie.release_date} </p>
+                    <div>
+                      <input className="add-library-button" type="button" value="Select This Movie" onClick={() => props.onSubmitMovieCallback(movie)} />
+                    </div>
                   </div>
                 </div>
                 </div>
