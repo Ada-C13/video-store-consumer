633a6e807ee922d4e9a7eba7094080b22bfff7e3
Maryam Heshmati <mairheshmati@gmail.com>
2020-06-24 14:43:19 -0700

    added functionality to checkout a movie when there is a selected user and movie

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 
 const Navbar = (props) => {
-  console.log(props);
+
+
+
+  const base_url = 'http://localhost:3000/rentals/';
+  const [checkedOutMovie, setCheckedOutMovie] = useState(null);
+  const [error, setError] = useState(null);
+  let statement;
+  const dueDate = new Date();
+  dueDate.setDate(dueDate.getDate() + 7);
+
+  if (props.selectedMovie && props.selectedCustomer) {
+   
+    const onButtonClick = (event) => {
+      event.preventDefault();
+
+      axios
+        .post(base_url + props.selectedMovie.title + '/check-out', {
+          customer_id: props.selectedCustomer.id, due_date: dueDate
+        })
+        .then((response) => {
+          setCheckedOutMovie(response.data);
+          console.log(response.data);
+        })
+        .catch((error) => {
+          setError(error.message);
+          console.log(error.message);
+        });
+    };
+
+    statement = <button onClick={onButtonClick}>Check Out</button>;
+  }
+  console.log(checkedOutMovie)
+ 
+
   return (
     <div className='Navbar'>
       <nav>
         <p>Selected Customer: {props.selectedCustomer.name}</p>
         <p>Selected Movie: {props.selectedMovie.title}</p>
+        <p>{statement}</p>
         <ul>
           <li>
             <Link to='/'>Home</Link>
