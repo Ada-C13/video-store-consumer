e02bacafe62cfc85edc21859e159116545f03767
Yieni <yaynee8d@gmail.com>
2020-06-23 19:56:10 -0700

    linked searchComponent

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import logo from './logo.svg';
 import './App.css';
 
 import Home from './components/Home';
-import Movies from './components/Movies';
+import SearchComponent from './components/SearchContainer';
 import Rentals from './components/Rentals';
 import Customers from './components/Customers';
 
@@ -14,21 +14,14 @@ class App extends Component {
     return (
     <Router>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         <ul>
           {/* https://www.golangprograms.com/how-to-create-simple-react-router-to-navigate-multiple-pages.html */}
           <li><Link to="/">Home</Link></li>
           <li><Link to="/search"> Search for Movies</Link></li>
-          <li><Link to="/library">Rentals</Link></li>
+          <li><Link to="/library">Library</Link></li>
           <li><Link to="/customers">Customers</Link></li>
           <Route exact path="/" component={Home} />
-          <Route exact path="/search" component={Movies} />
+          <Route exact path="/search" component={SearchComponent} />
           <Route exact path="/library" component={Rentals} />
           <Route exact path="/customers" component={Customers} />
         </ul> 
