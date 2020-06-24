bff748058c6252bf9a8f0dcb6eccf852c29d862f
Antonia <antonianomamiukor@gmail.com>
2020-06-24 11:21:53 -0700

    component for movie info page

---
+++ src/components/movieinfo.js
@@ -0,0 +1,19 @@
+// import React from 'react';
+// import PropTypes from 'prop-types';
+// import MovieIndex from './movieindex';
+
+
+// const SelectedMovie = ({movie}) => {
+//   return (
+//     <div>
+//       <h3>Selected movie: {movie.title}</h3>
+//       <img src={movie.image_url}  alt={movie.title}/>
+//     </div>
+//   );
+// }
+
+// export default SelectedMovie;
+
+// SelectedMovie.propTypes = {
+//   movie: PropTypes.object,
+// }
