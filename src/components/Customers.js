96e833008232043d595e1d0ff672aa66c49050c9
Haben <haben7@gmail.com>
2020-06-23 22:34:47 -0700

    add function to render customer list

--- src/components/Customers.js
+++ src/components/Customers.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
 
-const Customers = () => {
+
+
+
+
+const Customers = ({ API_URL_BASE }) => {
+  const [customerList, setCustomerList] = useState([]);
+  useEffect(() => {
+    axios.get(API_URL_BASE + "/customers").then((response) => {
+      // Load in the data
+      setCustomerList(response.data);
+    // }).catch((error) => {
+    // Show an error
+    // setError("There was an error with this request!");
+    });
+  },
+    []);
   return (
     <ol>
-      <li>First Customer</li>
-      <li>Second Customer</li>
-      <li>Third Customer</li>
-      <li>Fourth Customer</li>
+      {customerList.map((customer) =>
+      <li> {customer.name} </li>
+      )}
     </ol>
   )
 };
+
 
 export default Customers;
