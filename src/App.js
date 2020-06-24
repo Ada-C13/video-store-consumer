2d5080fe64793f55394bb3c777dc4485235d3e37
Vera <lizeth.veraro@outlook.com>
2020-06-23 19:53:32 -0700

    Get rid off extra info

--- src/App.js
+++ src/App.js
@@ -1,17 +1,6 @@
 
 import React, { Component } from 'react';
 import './App.css';
-import Store from './components/Store';
-import Library from './components/Library';
-
-
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-
 
 const App = () => {
 
