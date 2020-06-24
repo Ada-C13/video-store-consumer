4b41cbe43ca6fb66a9f3c3f8eb5cd735c375ec7e
Rosalyn <syrosalynyu@gmail.com>
2020-06-24 11:17:50 -0700

    moved the customers API call back to Customers

--- src/App.js
+++ src/App.js
@@ -10,52 +10,53 @@ import Library from './components/Library';
 
 
 const App = () => {
-  const API_URL_CUSTOMERS = "http://localhost:3000/customers"
+  // const API_URL_CUSTOMERS = "http://localhost:3000/customers"
 
-  const [customerList, setCustomerList] = useState([]);
-  const [errorMessage, setErrorMessage] = useState(null);
+  // const [customerList, setCustomerList] = useState([]);
+  // const [errorMessage, setErrorMessage] = useState(null);
 
-  useEffect(()=>{
-    axios.get(API_URL_CUSTOMERS)
-    .then((response) => {
-      const apiCustomerList = response.data;
-      console.log(response.data)
-      setCustomerList(apiCustomerList);
-    })
-    .catch((error) => {
-      setErrorMessage(error.message);
-      console.log(error.message);
-    });
-  }, []);
+  // // Get Cusomter list
+  // useEffect(()=>{
+  //   axios.get(API_URL_CUSTOMERS)
+  //   .then((response) => {
+  //     const apiCustomerList = response.data;
+  //     console.log(response.data)
+  //     setCustomerList(apiCustomerList);
+  //   })
+  //   .catch((error) => {
+  //     setErrorMessage(error.message);
+  //     console.log(error.message);
+  //   });
+  // }, []);
 
-  const customerComponents = customerList.map((customer) => {
-    return(
-      <Customers
-        name = {customer.name}
-        registeredAt = {customer.registerd_at}
-        address = {customer.address}
-        city = {customer.city}
-        state = {customer.state}
-        postalCode = {customer.postal_code}
-        phone = {customer.phone}
-        accountCredit = {customer.account_credit}
-        moviesCheckedOutCount = {customer.movies_checked_out_count}
-      />
-    );
-  })
+  // const customerComponents = customerList.map((customer) => {
+  //   return(
+  //     <Customers
+  //       name = {customer.name}
+  //       registeredAt = {customer.registerd_at}
+  //       address = {customer.address}
+  //       city = {customer.city}
+  //       state = {customer.state}
+  //       postalCode = {customer.postal_code}
+  //       phone = {customer.phone}
+  //       accountCredit = {customer.account_credit}
+  //       moviesCheckedOutCount = {customer.movies_checked_out_count}
+  //     />
+  //   );
+  // })
 
   return (
   <Router>
     <div className="App">
       <ul>
-        {/* https://www.golangprograms.com/how-to-create-simple-react-router-to-navigate-multiple-pages.html */}
+        {/* https://medium.com/the-andela-way/understanding-the-fundamentals-of-routing-in-react-b29f806b157e*/}
         <li><Link to="/">Home</Link></li>
         <li><Link to="/search"> Search for Movies</Link></li>
         <li><Link to="/library">Library</Link></li>
         <li><Link to="/customers">Customers</Link></li>
         <Route exact path="/" component={Home} />
         <Route exact path="/search" component={SearchComponent} />
-        <Route exact path="/library" component={Library} />
+        <Route exact path="/library" component={Library}/>
         <Route exact path="/customers" component={Customers} />
       </ul> 
     </div>
--- src/components/Customers.js
+++ src/components/Customers.js
@@ -1,20 +1,40 @@
 import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
-const Customers = ({name, registerdAt, address, city, state, postalCode, phone, accountCredit, moviesCheckedOutCount} ) => {
+const Customers = (props) => {
+  const API_URL_CUSTOMERS = "http://localhost:3000/customers"
+
+  const [customerList, setCustomerList] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  // Get Cusomter list
+  useEffect(()=>{
+    axios.get(API_URL_CUSTOMERS)
+    .then((response) => {
+      const apiCustomerList = response.data;
+      console.log(response.data)
+      setCustomerList(apiCustomerList);
+    })
+    .catch((error) => {
+      setErrorMessage(error.message);
+      console.log(error.message);
+    });
+  }, []);
 
   return (
     <div>
-      <div>
-        {name}
-        {registerdAt}
-        {address}
-        {city}
-        {state}
-        {postalCode}
-        {phone}
-        {accountCredit}
-        {moviesCheckedOutCount}
-      </div>
+      {customerList.map(customer => (
+        <div>
+          <h1>{customer.name}</h1>
+          <p>{customer.address}</p>
+          <p>{customer.city}</p>
+          <p>{customer.state}</p>
+          <p>{customer.postal_code}</p> 
+          <p>{customer.phone}</p>
+          <p>{customer.account_credit}</p>
+          <p>{customer.movies_checked_out_count}</p>
+        </div>
+      ))}
     </div>
   );
 }
