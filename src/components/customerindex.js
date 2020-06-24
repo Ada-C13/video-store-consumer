f52694916438a88843706991f5d144868c5b13a8
kate myer <kate.d.mangubat@gmail.com>
2020-06-23 20:19:11 -0700

    customer component set up

---
+++ src/components/customerindex.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import Customer from './Customer';
+import axios from 'axios';
+
+class CustomerIndex extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      customerList: [],
+      selectedCustomer: undefined
+    };
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:3000/customers').then((response) => {
+      this.setState({
+        customerList: response.data
+      })
+    }).catch(() => {
+      this.setState({
+        error: 'Error'
+      })
+    })
+  }
+
+  selectCustomer = (customerId) => {
+    const { customerList } = this.state;
+
+    const selectedCustomer = customerList.find((customer) => {
+      return customer.id === customerId;
+    });
+
+    this.setState({ selectedCustomer, });
+  }
+
+  render () {
+    const { selectedCustomer } = this.state;
+
+    const customers = this.state.customerList.map((customer, i) => {
+      return <Customer 
+        key={i}
+        id={customer.id}
+        name={customer.name}
+        registered_at={customer.registered_at}
+        address={customer.address}
+        city={customer.city}
+        state={customer.state}
+        postal_code={customer.postal_code}
+        phone={customer.phone}
+        account_credit={customer.account_credit}
+        selectCustomerCallback={this.selectCustomer}
+      />
+    })
+    return (
+      <div>
+        <h1>{this.state.error}</h1>
+        <h1>{selectedCustomer ? selectedCustomer.title : ''}</h1>
+          {customers}
+      </div>
+    );
+  }
+}
+
+export default CustomerIndex; 
