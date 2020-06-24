f2ff1eaff5a94c96e54c60e498ddf50fbbb33fb1
Alicia Combs <alicia.combs.92@gmail.com>
2020-06-24 10:53:45 -0700

    Updated logo and favicon.ico

--- src/components/Header.js
+++ src/components/Header.js
@@ -30,7 +30,7 @@ const Header = (props) => {
       <div>
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div class="navbar-brand float-left">
-          <a href="/"><img className="w-75" src="https://lh3.googleusercontent.com/DVogurPnvt3I4TyoyhNW7ZjhSMnyJGRlbVfaDE7UjkpbLNP40YQeRRlKDi8FWkMDSx2p00lRfcS0AZTqv6EEDn48nrJKKjW-Cvl9lCgO8v7UvKsgFO2JFHz3oHuusK3FzYlvUHnUnJYZxtkGKxVwlgorDA2IJza2Drq0wB-8q9Ob4B9w34WXFnx2ScbHrbnK5LzswTxT20lLBMMrHARzbZfO-e4AOcRln_w1LaYD7WW4JX_L5RfFhLG6OEHQ7MgT1hjQyLGY5ORcohhisLqRsJ79pJ-hAA-nNjS_A2VFkj3XnES_BX2nbA1rdZi1fMmzm4Lfz375p98bvdUrtoHZP8RWQPPAU42uRg9bc7Tl-IG6PzfhkHsOy6s2ebub8oD_Oa3zTYHmsCz0jrH4IJY42Gd9_Y-JN-7WJ7XOpt_eNvk2WJSh1zW1p1sxOXP0GzWBuRDwlMzHfRDq8-B3nFfAcP4QLF8vJfZAcxZwlO1I_HZKwYu6F_k4psIiZKXOXhyjWfUl_CUQWK_fBqeVM4nODb5Uq-OQckEoDhcveMpu33ddBdtzeBznb9pQ6eZenWHBcGplAPSX9Xx1Jaq7UooDtmW2tcE2kyl0_w0Z1mj1hrnsjk3JEc0lszc2HpXze-41yIBdZxzw9__DjFc95-L5Ani9jz0_7tcnwEGKy1enR7MxANc1wiB7kMRQW6XB4w=w400-h137-no?authuser=0"/></a>
+          <a href="/"><img className="w-75" src="https://i.imgur.com/7CsGGMu.png"/></a>
           </div>
           <ul className="navbar-nav mr-auto float-left">
             <li><Link to={'/'} className="nav-link">Home</Link></li>
