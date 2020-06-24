469c3bc88a8381564dc499fb0447b93b531bf9f7
Yieni <yaynee8d@gmail.com>
2020-06-23 17:41:06 -0700

    created a seach container component

---
+++ src/components/SearchContainer.js
@@ -0,0 +1,75 @@
+import React, { useState} from 'react';
+import axios from 'axios';
+
+const API_URL_MOVIES = "http://localhost:3000/movies"
+
+const SearchContainer = () => {
+  const [formFields, setFormFields] = useState({
+    text: ""
+  })
+  const [movieList, setMovieList] = useState([])
+
+  const [message, setMessage] = useState(null);
+
+  const onInputChange = (event) => {
+    const newFormFild = {...formFields}
+    newFormFild[event.target.name] = event.target.value
+    setFormFields(newFormFild)
+  }
+
+  const onFormSubmit = (event) =>{
+    event.preventDefault();
+
+    axios.get(API_URL_MOVIES, {
+      params: {
+        query: formFields.text
+      }
+    })
+    .then((response) => {
+      const apiMovieList = response.data;
+      console.log(apiMovieList)
+      setMovieList(apiMovieList);
+    })
+    .catch((error) => {
+      setMessage(error.message);
+      console.log(message);
+    });
+  }
+
+  const movieComponents = movieList.map((movie) => {
+    return(
+      <Movie
+        key = {movie.external_id}
+        external_id = {movie.external_id}
+        title = {movie.title}
+        overview = {movie.overview}
+        releaseDate = {movie.release_date}
+        imageUrl = {movie.image_url}
+      />
+    )
+  })
+
+  return(
+    <>
+      <form className="" onSubmit={onFormSubmit} >
+        <div className ="">
+          <label className="">Search for Movie Title: </label>
+          <input className = "" 
+            name="text"
+            value={formFields.text}
+            onChange = {onInputChange}
+            type="text"
+          />
+        </div>
+        <div>
+          <input type="submit" value="Find Movie" />
+        </div>
+      </form>
+      <div>
+        {movieComponents}
+      </div>
+    </>
+  )
+}
+
+export default SearchContainer;
