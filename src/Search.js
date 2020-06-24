d0335a3044371a21063fd6aa706ae1a218f46337
Yesenia <yesentorres@gmail.com>
2020-06-23 19:04:24 -0700

    first pass at implementing search feature

--- src/Search.js
+++ src/Search.js
@@ -1,10 +1,59 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
+import SearchResult from './SearchResult';
 
 const Search = () => {
+
+  const [searchTerm, setSearchTerm] = useState()
+  const [searchResults, setSearchResults] = useState([])
+  const [errorMessage, setErrorMessage] = useState(null);
+  
+  const onInputChange = (event) => {
+    setSearchTerm(event.target.value);
+    console.log(searchTerm);
+  }
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    // get list of search results from API and update state
+    const API_SEARCH_URL = `http://localhost:3000/movies?query=${searchTerm}`
+
+    axios.get(API_SEARCH_URL)
+      .then((response) => { 
+        setSearchResults(response.data);
+      })
+      .catch((error) => {
+        setErrorMessage(error.message);
+      });
+  
+
+    // turn each search result object into a search result component
+    const searchResultComponents = searchResults.map((resultsObj) => {
+      return(< SearchResult 
+        id={resultsObj.id}
+        title={resultsObj.title} 
+        image_url={resultsObj.image_url}
+        key={resultsObj.id}
+      />)
+    })
+
+  } // end onFormSubmit
+
   return (
     <div className="Search">
-      <h1>Search</h1>
+      <h1>Search Page</h1>
+
+      <p>Enter the movie you're looking for and we'll return every movie we know about that includes those words!</p>
+      <form onSubmit={onFormSubmit}>
+        <input onChange={onInputChange} value={searchTerm}/>
+
+        <input type="submit" value="Submit"/>
+      </form>
+
+      {searchResults}
+
     </div>
   );
 }
---
+++ src/SearchResult.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+const SearchResult = (props) => {
+
+  return (
+    <div>
+      <h3>I'm a Search Result</h3>
+    </div>
+  )
+}
+
+export default SearchResult;
