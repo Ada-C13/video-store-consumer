cb5b1458d729a92574394a07b200cc171bdaafd5
Rosalyn <syrosalynyu@gmail.com>
2020-06-24 13:35:10 -0700

    added Route to the movie detail page

--- src/App.js
+++ src/App.js
@@ -7,43 +7,10 @@ import Home from './components/Home';
 import SearchComponent from './components/SearchContainer';
 import Customers from './components/Customers';
 import Library from './components/Library';
+import Details from './components/Details';
 
 
 const App = () => {
-  // const API_URL_CUSTOMERS = "http://localhost:3000/customers"
-
-  // const [customerList, setCustomerList] = useState([]);
-  // const [errorMessage, setErrorMessage] = useState(null);
-
-  // // Get Cusomter list
-  // useEffect(()=>{
-  //   axios.get(API_URL_CUSTOMERS)
-  //   .then((response) => {
-  //     const apiCustomerList = response.data;
-  //     console.log(response.data)
-  //     setCustomerList(apiCustomerList);
-  //   })
-  //   .catch((error) => {
-  //     setErrorMessage(error.message);
-  //     console.log(error.message);
-  //   });
-  // }, []);
-
-  // const customerComponents = customerList.map((customer) => {
-  //   return(
-  //     <Customers
-  //       name = {customer.name}
-  //       registeredAt = {customer.registerd_at}
-  //       address = {customer.address}
-  //       city = {customer.city}
-  //       state = {customer.state}
-  //       postalCode = {customer.postal_code}
-  //       phone = {customer.phone}
-  //       accountCredit = {customer.account_credit}
-  //       moviesCheckedOutCount = {customer.movies_checked_out_count}
-  //     />
-  //   );
-  // })
 
   return (
   <Router>
@@ -58,6 +25,7 @@ const App = () => {
         <Route exact path="/search" component={SearchComponent} />
         <Route exact path="/library" component={Library}/>
         <Route exact path="/customers" component={Customers} />
+        <Route exact path= "/details/:title" component= {Details}/>
       </ul> 
     </div>
   </Router> 
