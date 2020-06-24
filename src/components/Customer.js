25b1c0d75a090d96aff0e3677d39b2369f29e797
peachmakkoli <lee.higgins.dev@gmail.com>
2020-06-23 23:04:57 -0700

    renders list of customers and data, dependent on which customer is clicked

--- src/components/Customer.js
+++ src/components/Customer.js
@@ -11,3 +11,22 @@ import {
 // import axios from 'axios';
 
 // import './Customer.css';
+
+const Customer = ({ id, name, registered_at, address, city, state, postal_code, phone, account_credit, movies_checked_out_count }) => {
+
+  return (
+    <section>
+      <h3>Customer #{id}: {name}</h3>
+      <p>Registered At: {registered_at}</p>
+      <p>Address: {address}</p>
+      <p>City: {city}</p>
+      <p>State: {state}</p>
+      <p>Postal Code: {postal_code}</p>
+      <p>Phone: {phone}</p>
+      <p>Account Credit: {account_credit}</p>
+      <p>Movies Checked Out: {movies_checked_out_count}</p>
+    </section>
+  );
+}
+
+export default Customer;
--- src/components/Customers.js
+++ src/components/Customers.js
@@ -13,8 +13,64 @@ import {
 // import './Customers.css';
 import Customer from './Customer';
 
-const Customers = () => {
-  return <h2>Customer List</h2>;
+const Customers = ({ list }) => {
+  let match = useRouteMatch();
+
+  const [ clickedCustomer, setClickedCustomer ] = useState(null);
+
+  const customerList = list.map(customer =>
+    <li key={customer.id}>
+      <Link to={`${match.url}/${customer.id}`} onClick={() => setClickedCustomer(customer)}>{customer.name}</Link>
+    </li>
+  );
+
+  if (clickedCustomer) {
+    return (
+      <section>
+        <h2>Customer List</h2>
+        
+        <ul>
+          {customerList}
+        </ul>
+
+        <Switch>
+          <Route path={`${match.path}/:customerId`}>
+            <Customer
+              name={clickedCustomer.name}
+              registered_at={clickedCustomer.registered_at}
+              address={clickedCustomer.address}
+              city={clickedCustomer.city}
+              state={clickedCustomer.state}
+              postal_code={clickedCustomer.postal_code}
+              phone={clickedCustomer.phone}
+              account_credit={clickedCustomer.account_credit}
+              movies_checked_out_count={clickedCustomer.movies_checked_out_count}
+              id={clickedCustomer.id}
+            />
+          </Route>
+          <Route path={match.path}>
+            <h3>Please select a customer.</h3>
+          </Route>
+        </Switch>
+      </section>
+    );
+  } else {
+    return (
+      <section>
+        <h2>Customer List</h2>
+        
+        <ul>
+          {customerList}
+        </ul>
+
+        <Switch>
+          <Route path={match.path}>
+            <h3>Please select a customer.</h3>
+          </Route>
+        </Switch>
+      </section>
+    );
+  }
 }
 
 export default Customers;
