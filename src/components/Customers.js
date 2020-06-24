404ebaa0582796ef19453ae78bcf90395e6f7b0d
Haben <haben7@gmail.com>
2020-06-23 19:16:46 -0700

    add Customers component

---
+++ src/components/Customers.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Customers = () => {
+  return (
+    <ol>
+      <li>First Customer</li>
+      <li>Second Customer</li>
+      <li>Third Customer</li>
+      <li>Fourth Customer</li>
+    </ol>
+  )
+};
+
+export default Customers;
