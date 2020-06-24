1707447e9b9d67c41af9256280b7e6856e80683f
Yieni <yaynee8d@gmail.com>
2020-06-23 22:33:11 -0700

    created library

---
+++ src/components/Library.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Movie from "./Movie";
+
+const API_URL_MOVIES = "http://localhost:3000/movies"
+
+const Library = () => {
+  const [movieList, setMovieList] = useState([])
+
+  const [message, setMessage] = useState(null);
+
+  // event.preventDefault();
+  useEffect(()=>{
+    axios.get(API_URL_MOVIES)
+    .then((response) => {
+      const apiMovieList = response.data;
+      console.log(apiMovieList)
+      setMovieList(apiMovieList);
+    })
+    .catch((error) => {
+      setMessage(error.message);
+      console.log(message);
+    });
+  }, []);
+
+  const movieComponents = movieList.map((movie) => {
+    return(
+      <Movie
+        key = {movie.external_id}
+        externalId = {movie.external_id}
+        title = {movie.title}
+        overview = {movie.overview}
+        releaseDate = {movie.release_date}
+        imageUrl = {movie.image_url}
+        showButton = {false}
+      />
+    )
+  })
+  return (
+    <div>
+      {movieComponents}
+    </div>
+  );
+}
+
+export default Library;
