452aecfbc3925aa7444eaf07395b8f0f1c01ffaf
Alicia Combs <alicia.combs.92@gmail.com>
2020-06-23 19:42:53 -0700

    Added logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -28,16 +28,17 @@ const Header = (props) => {
   return (
   <Router>
       <div>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light justify-content-between">
-          <h2 className="">Videos</h2>
-
-          <ul className="navbar-nav mr-auto">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="navbar-brand float-left">
+          <a href="/"><img className="w-75" src="https://lh3.googleusercontent.com/DVogurPnvt3I4TyoyhNW7ZjhSMnyJGRlbVfaDE7UjkpbLNP40YQeRRlKDi8FWkMDSx2p00lRfcS0AZTqv6EEDn48nrJKKjW-Cvl9lCgO8v7UvKsgFO2JFHz3oHuusK3FzYlvUHnUnJYZxtkGKxVwlgorDA2IJza2Drq0wB-8q9Ob4B9w34WXFnx2ScbHrbnK5LzswTxT20lLBMMrHARzbZfO-e4AOcRln_w1LaYD7WW4JX_L5RfFhLG6OEHQ7MgT1hjQyLGY5ORcohhisLqRsJ79pJ-hAA-nNjS_A2VFkj3XnES_BX2nbA1rdZi1fMmzm4Lfz375p98bvdUrtoHZP8RWQPPAU42uRg9bc7Tl-IG6PzfhkHsOy6s2ebub8oD_Oa3zTYHmsCz0jrH4IJY42Gd9_Y-JN-7WJ7XOpt_eNvk2WJSh1zW1p1sxOXP0GzWBuRDwlMzHfRDq8-B3nFfAcP4QLF8vJfZAcxZwlO1I_HZKwYu6F_k4psIiZKXOXhyjWfUl_CUQWK_fBqeVM4nODb5Uq-OQckEoDhcveMpu33ddBdtzeBznb9pQ6eZenWHBcGplAPSX9Xx1Jaq7UooDtmW2tcE2kyl0_w0Z1mj1hrnsjk3JEc0lszc2HpXze-41yIBdZxzw9__DjFc95-L5Ani9jz0_7tcnwEGKy1enR7MxANc1wiB7kMRQW6XB4w=w400-h137-no?authuser=0"/></a>
+          </div>
+          <ul className="navbar-nav mr-auto float-left">
             <li><Link to={'/'} className="nav-link">Home</Link></li>
             <li><Link to={'/library'} className="nav-link">Library</Link></li>
             <li><Link to={'/customers'} className="nav-link">Customers</Link></li>
             <li><Link to={'/search'} className="nav-link">Search</Link></li>
           </ul>
-
+          <div className="w-25"></div>
           <div className="selected w-75 d-flex justify-content-end">
             <ul className="list-group list-group-horizontal text-center">
               <li className={ selectedCustomer ? "list-group-item active" : "list-group-item text-muted"}>{selectedCustomer ? selectedCustomer.name : "No Customer Selected"} </li>
@@ -47,7 +48,7 @@ const Header = (props) => {
           </div>
 
         </nav>
-        <hr />
+        
         <Switch> 
             <Route exact path='/' component={Home} />
             <Route 
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 const Home = () => {
   return (
-      <div>
-        <h2>Home</h2>
+      <div className="card rounded-0 home-card">
+        <img className="w-100 card-img" src="https://lh3.googleusercontent.com/FHWIecUOrFFGfQak-MAZ6KuXubYH576GlVjcJUujQGcUFdIDd3iqngSNIzgJyjKq-HM3B-1YqUK2gC6T1AIln-fA_E7xvnLN0KZMyNQ0Oqi3MLxOQc_IrCj4QMoOKWzRCQApXrzI4mou9aMs6nmaVdYhRfHQ511n9be2X0CyJs73xlKX6pNbjEDnL7GnDWmRG-Q0qimUwC2sFzUgLDeC7MF5kU-xJAPCsd_fwkrIzmS_65wF88I7duAcvC5qvXqtjD-9hr-jsHbj-3tl7VAs2FF2MA2YoKJ9705NdBMQhDZLvg_fqnnCoNHkBrd5qCpjUK7z4zr4SJr-x2ADnTDEyT7Qdf6Rfw8nmPJCz_MHpJ_f5yg5jooE6n14SO70kxiopnntWhdIjPQ21kQY0KynYhvf7vb5HOIcgN2vaME1vQqv3e-npNEsA_4g2aOuuU9tQP5sBWc0hXXCC_iHAUSj_2wlA-1qAWkBMAgyEzWa-YGJ-W5oQHQwNVg35jeqctNFnhBbUmk97HPcQmPX52BATRiBMBpNHgI5y7AKa-sRYxWwQYCnZRtyNhyBCO4M1e7xmJoQMFMAxExKyGHq8HVOF-QxeqpFZE_O71YFiMmDufweH6f6gW1y-Kc4xTdlNS6a0YuzSISAdx-UMqtAuGb0F0mdyZe_8ehYL014dHxvYrtRcIm8fKLJy_ZSj0b9VQ=w1646-h1096-no?authuser=0"/>
+
+        <div class="card-img-overlay align-content-around not-found">
+          <h2 class="card-title text-center justify-content-around mt-2"></h2>
+        </div>
       </div>
   );
 }
