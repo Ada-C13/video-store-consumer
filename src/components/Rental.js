667b9f8f43c7fa1ca8ca01fce6bf4aaa5b298657
Olga Patrakova <olgapatrakova@outlook.com>
2020-06-24 16:16:04 -0700

    Rental info disappear as soon as we click Rent

--- src/components/Rental.js
+++ src/components/Rental.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Rental.css';
 import PropTypes from 'prop-types';
 
 const Rental = ({ movie, customer, rentMovie }) => {
+  const [isClickedOn, setIsClickedOn] = useState(true);
+
+  if (isClickedOn) {
   return (
     <div>
       { movie && <p> Selected Movie: { movie.title }, Available inventory: {movie.available_inventory} </p> }
       { customer && <p> Selected Customer: { customer.name } </p> }
-      { (movie && customer) && <button onClick={() => { rentMovie() }}>Rent Movie</button>} 
+      { movie && customer ? <button onClick={() => { rentMovie(); setIsClickedOn(false) }}>Rent Movie</button> : ""} 
     </div>
-  );
-};
+    );
+  };
+  return ("");
+}
+
 
 Rental.propTypes = {
   movie: PropTypes.object,
@@ -18,4 +24,3 @@ Rental.propTypes = {
 }
 
 export default Rental;
-
