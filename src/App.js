fd5eef07cf725a4b242a1ddc708f77c3d6756097
Haben <haben7@gmail.com>
2020-06-23 19:16:16 -0700

    add import React router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React, { useState } from 'react';
 import AppHeader from './components/AppHeader'
+import Customers from './components/Customers'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 import './App.css';
 
 const App = () => {
@@ -7,20 +15,56 @@ const App = () => {
   const incrementCounter = () => {
     setCounter(counter + 1);
   };
-  
+
   return (
     <div className="App">
-      <AppHeader 
-        text="Click Refresher in React" 
+      <AppHeader
+        text="Click Refresher in React"
         counter={counter}
-        clickCallback={incrementCounter} 
+        clickCallback={incrementCounter}
         />
       <p className="App-intro">
         To get started, edit <code>src/App.js</code> and save to reload.
       </p>
+
+      <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/customers">Customers</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+    <Switch>
+          <Route path="/customers">
+            <Customers />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
     </div>
   );
 };
- 
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
 
 export default App;
