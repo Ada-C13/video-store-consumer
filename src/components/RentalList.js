2788995380d289b4d0eddc117baad55f4d3bd0d2
Jocelyn Wang <wangjoc@gmail.com>
2020-06-24 07:51:29 -0700

    added in list of rentals

---
+++ src/components/RentalList.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Rental from './Rental';
+import './RentalList.css';
+
+const RentalList = ({ rentalList, selectRental, returnRental }) => {
+  const buildRentals = () => {
+    console.log (rentalList)
+    const rentalElements = rentalList.map((rental) => {
+      return <Rental 
+        key={rental.id}
+        { ...rental }
+        selectRental={(id) => selectRental(id)}
+        returnRental={(movie, customer) => returnRental(movie, customer)}
+      />
+    });
+
+    return rentalElements;
+  }
+
+  return (
+    <div>
+    <div>
+ 
+    </div>
+      <h1>RENTAL INFORMATION</h1>
+      <p>Select rental to return it.</p>
+      {buildRentals()}
+    </div>
+  )
+}
+
+RentalList.propTypes = {
+  rentalList: PropTypes.array.isRequired,
+  selectCustomer: PropTypes.func.isRequired,
+};
+
+export default RentalList;
