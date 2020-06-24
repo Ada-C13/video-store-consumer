671470eae5e9aba376e69f776a4909aa3fc95b89
Yaz <yasmineoshaugh@gmail.com>
2020-06-24 16:53:46 -0700

    cleaned up some import statements, made changes to how key is being set for each movie.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -8,28 +8,28 @@ import {
   useParams
 } from "react-router-dom";
 import PropTypes from 'prop-types';
-import Popup from "reactjs-popup";
+
 
 // import axios from 'axios';
 
-// import './Movies.css';
+import './Movies.css';
 import Movie from './Movie';
 
 const Movies = (props) => {
   const movieList = props.list.map((movie, i) => {
     return (
-      <li key={i}>
-        <div className="movie-list-item">
+      <div key={i}>
           <Movie 
-            // image ???
+            // image ??? >> in the api there is an image method for 
+            // movie but it doesn't seem like it is made available by the api as is
             title={movie.title} 
             overview={movie.overview} 
             release_date={movie.release_date}
             inventory={movie.inventory}
             onSelectCallback={props.onSelectCallback}
+            id={movie.id}
           />
-        </div>
-      </li>
+      </div>
     );
   });
   return (
