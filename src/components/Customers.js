ceaf0e489526971dab43cbf0bb94fc6b65e4d72a
Yieni <yaynee8d@gmail.com>
2020-06-23 22:35:52 -0700

    need to fix customer page and api call. deleted rentals

--- src/components/Customers.js
+++ src/components/Customers.js
@@ -1,35 +1,20 @@
-import React, {useState} from 'react';
-import axios from 'axios';
+import React, {useState, useEffect} from 'react';
 
-const API_URL_CUSTOMERS = "http://http://localhost:3000/customers"
-
-const Customers = () => {
-
-  const [customerList, setCustomerList] = useState([]);
-  const [errorMessage, setErrorMessage] = useState(null);
-
-  
-    axios.get(API_URL_CUSTOMERS)
-    .then((response) => {
-      const apiCustomerList = response.data;
-      setCustomerList(apiCustomerList);
-    })
-    .catch((error) => {
-      setErrorMessage(error.message);
-      console.log(error.message);
-    });
-
+const Customers = ({name, registerdAt, address, city, state, postalCode, phone, accountCredit, moviesCheckedOutCount} ) => {
 
   return (
     <div>
-      <h1>Customer</h1>
-        <p>customer list page: lists customers
-        1.) On the customer list page, I can see a list of all customers
-        2.) From the customer list, I can "select" a customer. My customer selection will continue to be visible as I navigate the app.
-        </p>
-        <ul>
-          <li>{customerList}</li>
-        </ul>
+      <div>
+        {name}
+        {registerdAt}
+        {address}
+        {city}
+        {state}
+        {postalCode}
+        {phone}
+        {accountCredit}
+        {moviesCheckedOutCount}
+      </div>
     </div>
   );
 }
