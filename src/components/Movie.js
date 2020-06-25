1cc42634fc03f6fd689d71a7a28913fc1bdabe34
Rosalyn <syrosalynyu@gmail.com>
2020-06-25 16:01:11 -0700

    removed unneeded buttons from Movie.js

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,17 +1,15 @@
 import React,{useState} from 'react';
-import { BrowserRouter as Router, Link, Route, Switch, useRouteMatch,} from 'react-router-dom';
 import axios from 'axios';
-import Selected from './Selected'
 import "./Movie.css"
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 const API_URL_MOVIES = "http://localhost:3000/movies"
 
 const Movie = ({externalId, title, overview, releaseDate, imageUrl, showAddButton, showDetailButton, selectMovieButton}) => {
   const [message, setMessage] = useState(null);
-  const [selectedMovie, setSelectedMovie] = useState(null)
   
   const addMovie = (event) =>{
     event.preventDefault();
@@ -31,22 +29,9 @@ const Movie = ({externalId, title, overview, releaseDate, imageUrl, showAddButto
       setMessage(`${title} already exists in the library!`)
     });
   }
-
-  const selectMovie = (event) =>{
-    event.preventDefault();
-    setSelectedMovie(title);
-  }
-
-  const selectedMovieComponents = () => {
-    return(
-      <Selected
-        movieTitle = {selectedMovie}
-      />
-    )
-  };
   
   return (
-   <div className="outer-card">
+    <div className="outer-card">
       <div className='card-container'>
         <img src={imageUrl} alt={title +"poster"}></img>
         <div className='card-info'>
@@ -61,18 +46,11 @@ const Movie = ({externalId, title, overview, releaseDate, imageUrl, showAddButto
       </div>
       <div className="card-buttons">
         {showAddButton && <Button variant="outline-secondary" onClick={addMovie}>Add to Movie Library</Button>}
-        {showDetailButton && <Link to={`/details/${title}`}><Button variant="outline-secondary">Details</Button></Link>}
-        <br/>
-        {selectMovieButton && <Button variant="outline-secondary" onClick={selectMovie}>Select this Movie</Button>}
       </div>
       <div>
-       {message}
-        {false && selectedMovieComponents()}
+        {message}
       </div>
-   </div>
-    
-    
-    
+    </div>
   );
 }
 
