368b442b58544ccc8527c5ba1912e761416e5088
Alicia Combs <alicia.combs.92@gmail.com>
2020-06-24 19:17:35 -0700

    Cleaned up app.js of comments

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
 
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Header from './components/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const App = () => {
-  useEffect(() => {
-    
-  }, [])
-  //create a callback func for customers...to set the selected customer
-  //use effect statement to store selectedCustomer locally
-  //add func to clear it
-  //add func to set it to a new one
-
-
     return (
       <section>
-        <Header 
-        // selectedMovie={selectedMovie}
-          // selectedCustomer={selectedCustomer}
-          // selectCustomerCallback={selectCustomer}
-          // url={URL}
-         />
+        <Header />
       </section>
     );
 }
