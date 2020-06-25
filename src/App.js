8fd6800489d5d4b4a519ae9656d1d184e43ef4b7
Rosalyn <syrosalynyu@gmail.com>
2020-06-25 16:55:15 -0700

    added api calls in App.js PLUS callbacks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component, useState, useEffect } from 'react';
 import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 import './App.css';
-
+import axios from 'axios';
 import Home from './components/Home';
 import SearchComponent from './components/SearchContainer';
 import Customers from './components/Customers';
@@ -12,15 +12,55 @@ import Selected from './components/Selected';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
 // http://localhost:3000/rentals/{title}/check-out 
 const App = () => {
   const [currentCustomer, setCurrentCustomer] = useState(null);
   const [currentMovie, setCurrentMovie] = useState(null);
+  const [message, setMessage] = useState(null);
 
   const updateMovie = (selectedMovie) => {
     setCurrentMovie(selectedMovie)
   }
-  
+
+  const updateCustomer = (selectedCustomer) => {
+    setCurrentCustomer(selectedCustomer)
+  }
+
+  const API_URL_CHECKOUT = `http://localhost:3000/rentals/${currentMovie}/check-out`
+  const checkOut = (event) =>{
+    event.preventDefault();
+
+    axios.post(API_URL_CHECKOUT, {
+      customer_id: currentCustomer
+    })
+    .then(() => {
+      setMessage(`Customer with ID ${currentCustomer} successfully checked out ${currentMovie}!`)
+    })
+    .catch((error) => {
+      console.log(error.message)
+      setMessage(`Failed to checkout!`)
+    });
+  }
+
+  const API_URL_RETURN = `http://localhost:3000/rentals/${currentMovie}/return`
+  const returnMovie = (event) =>{
+    event.preventDefault();
+
+    axios.post(API_URL_RETURN, {
+      customer_id: currentCustomer
+    })
+    .then(() => {
+      setMessage(`Customer with ID ${currentCustomer} successfully returned ${currentMovie}!`)
+    })
+    .catch((error) => {
+      console.log(error.message)
+      setMessage(`Failed to return!`)
+    });
+  }
+
+
+
   return (
     <>
       <div className="header">
@@ -30,8 +70,10 @@ const App = () => {
       <div className="note">
         <p>Selected Customer: {currentCustomer}</p>
         <p>Selected Movie: {currentMovie}</p>
-        <Button variant="outline-secondary" >Check-out</Button>
-        <Button variant="outline-secondary">Return</Button>
+        <Button variant="outline-secondary" onClick={checkOut}>Check-out</Button>
+        <Button variant="outline-secondary" onClick={returnMovie}>Return</Button>
+        <br/>
+        {message}
       </div>
       
       <Router>
@@ -45,9 +87,10 @@ const App = () => {
           <div>
             <Route exact path="/" component={Home} />
             <Route exact path="/search" component={SearchComponent} />
-            <Route exact path="/library" 
+            <Route exact path="/library"
             render={(props) => <Library onUpdateCurrentMovie={updateMovie}/>}/>
-            <Route exact path="/customers" component={Customers} />
+            <Route exact path="/customers"
+            render={(props) => <Customers onUpdateCurrentCustomer={updateCustomer}/>}/>
             <Route exact path= "/details/:title" component= {Details}/>
           </div>
       </Router> 
