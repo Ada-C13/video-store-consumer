59f5b07242b4a80afb3839fb132d1050506f8036
Leah Hughes <leahwhughes@gmail.com>
2020-06-25 11:11:46 -0700

    adds selectedCustomer state, updateSelected method

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { useState, Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Home from "./components/Home";
 import Library from './components/Library';
-import Customers from './components/CustomersList';
+import CustomersList from './components/CustomersList';
 import Search from './components/Search';
 import {
   BrowserRouter as Router,
@@ -11,59 +11,82 @@ import {
   Link
 } from "react-router-dom";
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <head>
-          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" 
-          integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" 
-          crossorigin="anonymous"></link>
-        </head>
-          
-        <nav className="navbar navbar-expand-lg navbar-dark px-5">
-          <a className="navbar-brand text-uppercase" href="/">Rentflix</a>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
+const App = () => {
 
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item">
-                <a className="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/search">Search <span class="sr-only">(current)</span></a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/library">Library <span class="sr-only">(current)</span></a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/customers">Customers <span class="sr-only">(current)</span></a>
-              </li>
-            </ul>
-          </div>
-        </nav>
+  const [selectedCustomer, setSelectedCustomer] = useState({
+    name: '',
+    id: null,
+  });
 
-        <Switch>
-          <Route path="/search">
-            <Search />
-          </Route>
-          <Route path="/customers">
-            <Customers />
-          </Route>
-          <Route path="/library">
-            <Library />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
 
-      </Router>
-    );
-  }
-}
+  const updateSelectedCustomer = (newSelected) => {
+    setSelectedCustomer({
+      name: newSelected.name,
+      id: newSelected.id
+    });
+    // setAlert(`You selected ${newSelected.name}. Nice.`)
+  };
+
+  return (
+    <Router>
+      <head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" 
+        integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" 
+        crossorigin="anonymous"></link>
+      </head>
+        
+      <nav className="navbar navbar-expand-lg navbar-dark px-5">
+        <a className="navbar-brand text-uppercase" href="/">Rentflix</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/search">Search <span class="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/library">Library <span class="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/customers">Customers <span class="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Selected Movie: {selectedMovie.title} <span class="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Selected Customer: {selectedCustomer.name} <span class="sr-only">(current)</span></a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <Switch>
+        <Route path="/search">
+          <Search />
+        </Route>
+        <Route path="/customers">
+          <CustomersList 
+            onUpdateSelectedCustomer={updateSelectedCustomer} 
+            selectedCustomer={selectedCustomer.id} 
+          />
+        </Route>
+        <Route path="/library">
+          <Library />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+
+    </Router>
+  );
+ }
+
 
 
 export default App;
