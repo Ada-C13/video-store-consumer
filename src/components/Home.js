10c3a9d1ff33683ec3d113f9b7259f2fed3fecbd
Alicia Combs <alicia.combs.92@gmail.com>
2020-06-24 21:52:12 -0700

    Added footer

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 const Home = () => {
   return (
-        <div class="text-center container">
-          <h2 class="text-center justify-content-around pt-5">Welcome to LA Videos!</h2>
-          <p></p>
+      <div className="card home-card rounded-0 home-card">
+        <img className="w-100 card-img" src='https://lh3.googleusercontent.com/pw/ACtC-3fD4ohzhSj8UxpU5P_nJIF7tnnfeyfjrbQMEcJXmH_k1iMrieKF4YdlCkP0jycCA4SepIhh3ad_m78vYRMIWdzI3URoCKMaTMSNxYxhK_as-Pc_K0vgkf096Zvm8uUNFC4ksqJg222EDaNYi2IC80OF=w1590-h724-no?authuser=0'/>
+
+        <div class="card-img-overlay text-center caption-6 d-flex justify-content-center align-items-center flex-column">
+          <h2 class="card-title text-center justify-content-around mt-2"></h2>
+        </div>
       </div>
   );
 }
