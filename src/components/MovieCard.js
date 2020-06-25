187df835eaaa6500beda08a71d9e6263e7794ed8
Leah Hughes <leahwhughes@gmail.com>
2020-06-25 14:24:03 -0700

    adds onSelect to movieCard

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,46 +1,25 @@
 import React, {useState} from 'react';
-import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card'
 import PropTypes from "prop-types";
 
-const MovieCard = ({title, external_id, overview, release_date, image_url}) => {
+const MovieCard = ({id, title, external_id, overview, image_url, onUpdateSelect, selected_id}) => {
 
-  const [error, setError] = useState(null);
+  const onSelect = () => {
+    const newSelected = {
+      id: id,
+      name: title,
+    }
+    onUpdateSelect(newSelected)
+  }
 
-
-  const selectMovie = (event) => {
-    // POST request
-    // event.preventDefault();
-    // axios.post('http://localhost:3000/movies', {
-    //   params: {
-    //     external_id: external_id,
-    //     title: title,
-    //     overview: overview,
-    //     release_date: release_date,
-    //     image_url: image_url
-    //   }
-    // })
-    // .then((response) => {
-    //   const data = response.data;
-    //   console.log(`this is response.data ${response.data}`);
-    //   console.log(`this is movies list before: ${moviesList}`);
-    //   let movieCollection = data.map((movie) => {
-    //     return (
-    //       <MovieCard
-    //       external_id = {movie.external_id}
-    //       title = {movie.title}
-    //       overview = {movie.overview}
-    //       release_date = {movie.release_date}
-    //       image_url = {movie.image_url}
-    //       />
-    //       );
-    //     });
-    //     setMoviesList(movieCollection);
-    //   })
-    //   .catch((error) => {
-    //     setError(error.response);
-    //   });
-  };
+  const selected = () => {
+    console.log(selected_id)
+    if (id === selected_id) {
+      return true
+    } else {
+      return false
+    }
+  }
 
   return (
       <Card style={{ width: '24rem' }}>
@@ -48,16 +27,11 @@ const MovieCard = ({title, external_id, overview, release_date, image_url}) => {
         <Card.Body>
           <Card.Title>{title}</Card.Title>
           <Card.Text>{overview}</Card.Text>
-          <button className="button" onClick={selectMovie}>Select this Movie</button>
+          <button className="button" onClick={onSelect}> {selected()? 'Selected' : 'Select this Movie'}</button>
         </Card.Body>
       </Card>
     )
   };
-
-
-
-
-
 
 MovieCard.propTypes = {
   external_id: PropTypes.number,
@@ -67,6 +41,5 @@ MovieCard.propTypes = {
   image_url: PropTypes.string
 };
 
-// title, overview, release_date, image_url, external_id
 
 export default MovieCard;
