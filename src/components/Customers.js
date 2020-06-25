75ab72a7d7a2627ea016cbbf17d76cdea9e8df0c
Yieni <yaynee8d@gmail.com>
2020-06-24 19:07:21 -0700

    imported selected, added a select customer event handler and a button

--- src/components/Customers.js
+++ src/components/Customers.js
@@ -1,18 +1,19 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
+import Selected from './Selected'
 
-const Customers = (props) => {
+const Customers = () => {
   const API_URL_CUSTOMERS = "http://localhost:3000/customers"
 
   const [customerList, setCustomerList] = useState([]);
   const [errorMessage, setErrorMessage] = useState(null);
+  const [selectedCustomerId, setSelectedCustomerId] = useState(null)
 
   // Get Cusomter list
   useEffect(()=>{
     axios.get(API_URL_CUSTOMERS)
     .then((response) => {
       const apiCustomerList = response.data;
-      console.log(response.data)
       setCustomerList(apiCustomerList);
     })
     .catch((error) => {
@@ -20,12 +21,27 @@ const Customers = (props) => {
       console.log(error.message);
     });
   }, []);
+  
+  const onSelectCustomer = (event) =>{
+    event.preventDefault();
+    setSelectedCustomerId(event.target.value);
+  }
+  console.log(selectedCustomerId)
+
+  const selectedComponents = () => {
+    return(
+      <Selected
+        customerId = {selectedCustomerId}
+      />
+    )
+  };
 
   return (
     <div>
       {customerList.map(customer => (
         <div>
           <h1>{customer.name}</h1>
+          <p>{customer.id}</p>
           <p>{customer.address}</p>
           <p>{customer.city}</p>
           <p>{customer.state}</p>
@@ -33,6 +49,8 @@ const Customers = (props) => {
           <p>{customer.phone}</p>
           <p>{customer.account_credit}</p>
           <p>{customer.movies_checked_out_count}</p>
+          <button value = {customer.id} onClick={onSelectCustomer}>Select this Customer</button>
+          {false && selectedComponents()}
         </div>
       ))}
     </div>
