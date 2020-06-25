8346fbe1caaa332c8adf1bd281b0b7d6f7695371
denisseai <msdenisseanaya@gmail.com>
2020-06-24 22:04:11 -0700

    updated customer css

--- src/components/Customer.js
+++ src/components/Customer.js
@@ -5,34 +5,30 @@ import { Card, Button } from 'react-bootstrap/';
 
 const Customer = ({ id, name, address, city, state, postalCode, registered_at, phone, account_credit, movies_checked_out_count, selectCustomer }) => {
   return (
-    <Card className="customer-card-container">
-      <img src={`https://image.flaticon.com/icons/svg/3105/310593${id%10}.svg`} alt="avatar thumbnail" className="img-thumbnail w-25"/>
-      <div className="customer-card">
-        <div className="customer-name">
-          <h3> {name} </h3>
-          <ul>
-            <li>Movies Checked Out: {movies_checked_out_count} </li>
-            <li>Account Credit: ${account_credit} </li>
-          </ul>
+    <Card>
+      <div className="col customer-card-container">
+        <div className="row customer-card">
+          <img src={`https://image.flaticon.com/icons/svg/3105/310593${id%10}.svg`} alt="avatar thumbnail" className="img-thumbnail w-25"/>
+          <div className="customer-name">
+            <h3> {name} </h3>
+            <ul>
+              <li>Movies Checked Out: {movies_checked_out_count} </li>
+              <li>Account Credit: ${account_credit} </li>
+              <li>Registration Date: {(registered_at).substring(0,10)} </li>
+              <li>Phone: {phone} </li>
+              <li>Address: {address}, {city}, {state} {postalCode} </li>
+              <div className="customer-select-button">
+              <Button
+                varient="flat"
+                type="button"
+                className="select-customer"
+                onClick={() => selectCustomer(id)}>
+                  Select
+              </Button>
+              </div>
+            </ul>
+          </div>
         </div>
-
-        <div className="customer-info">
-          <ul> 
-            <li>Registration Date: {(registered_at).substring(0,10)} </li>
-            <li>Phone: {phone} </li>
-            <li>Address: {address}, {city}, {state} {postalCode} </li>
-          </ul>
-        </div>
-
-        <div className="customer-select-button">
-          <Button
-            varient="flat"
-            type="button"
-            className="select-customer"
-            onClick={() => selectCustomer(id)}>
-              Select
-          </Button>
-        </div>  
       </div>
     </Card>
   )
