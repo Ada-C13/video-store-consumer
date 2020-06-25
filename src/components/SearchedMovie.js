db54bb0c7b270db00c07620e98b26ca58ac2b877
Jetmire Bajrami <jetmirebajrami@Jetmires-MacBook-Pro.local>
2020-06-24 18:17:34 -0700

    Added searchmovie functionality

---
+++ src/components/SearchedMovie.js
@@ -0,0 +1,27 @@
+import './Movie.css';
+import React from 'react';
+import { Button } from 'react-bootstrap/';
+
+const SearchedMovie = ({ id, title, overview, release_date, image_url, external_id}) => {
+  return (
+  <div className="">
+    <div className="">
+      <h3> {title} </h3>
+      <ul>
+        <li>Overview: {overview} </li>
+        <li>Release Date: {release_date} </li>
+        <li><img src={image_url} alt={title}/></li>
+        <li>External id: {external_id} </li>
+      </ul>
+    </div>
+    <div className="">
+      <Button
+        className="">
+          Add Movie
+      </Button> 
+    </div>  
+  </div>
+  )
+};
+
+export default SearchedMovie;
