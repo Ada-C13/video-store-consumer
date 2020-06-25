0ff5f2917bbbfbe1e0bda6960238ee862913ac40
Leah Hughes <leahwhughes@gmail.com>
2020-06-25 11:09:40 -0700

    adds useEffect to prevent 1,000,000 API calls

--- src/components/CustomersList.js
+++ src/components/CustomersList.js
@@ -1,34 +1,39 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from "axios";
 import CustomerCard from './CustomerCard';
 
-const CustomersList = () => {
+const CustomersList = ({onUpdateSelectedCustomer, selected_id}) => {
   const [error, setError] = useState(null);
   const [customerList, setCustomerList] = useState(null);
 
-  axios.get('http://localhost:3000/customers')
-    .then((response) => {
-      const data = response.data;
-      console.log(`this is response.data ${response.data}`);
-      let customerCollection = data.map((customer) => {
-        return (
-          <div className="col-md-4 pb-4">
-            <CustomerCard
-              id = {customer.id}
-              name = {customer.name}
-              movies_checked_out_count = {customer.movies_checked_out_count}
-              registered_at = {customer.registered_at}
-              phone = {customer.phone}
-              key = {customer.id}
-            />
-          </div>
-        );
-      });
-      setCustomerList(customerCollection);
-    })
-    .catch((error) => {
-      setError(error.response);
-    });
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/customers')
+      .then((response) => {
+        const data = response.data;
+        console.log(`this is response.data ${response.data}`);
+        let customerCollection = data.map((customer) => {
+          return (
+            <div className="col-md-4 pb-4">
+              <CustomerCard
+                id = {customer.id}
+                name = {customer.name}
+                movies_checked_out_count = {customer.movies_checked_out_count}
+                registered_at = {customer.registered_at}
+                phone = {customer.phone}
+                key = {customer.id}
+                selected_id={selected_id}
+                onUpdateSelect={onUpdateSelectedCustomer}
+              />
+            </div>
+          );
+        });
+        setCustomerList(customerCollection);
+      })
+      .catch((error) => {
+        setError(error.response);
+      })
+   }, []);
 
   return (
     <div class="row m-5 pt-5">{customerList}</div>
--- src/components/Library.js
+++ src/components/Library.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from "axios";
 import MovieCard from './MovieCard';
 
@@ -6,29 +6,31 @@ const Library = () => {
   const [error, setError] = useState(null);
   const [libraryList, setLibraryList] = useState(null);
 
-  axios.get('http://localhost:3000/movies')
-    .then((response) => {
-      const data = response.data;
-      console.log(`this is response.data ${response.data}`);
-      let movieCollection = data.map((movie) => {
-        return (
-          <div className="col-md-4 pb-4">
-            <MovieCard
-            external_id = {movie.external_id}
-            title = {movie.title}
-            overview = {movie.overview}
-            release_date = {movie.release_date}
-            image_url = {movie.image_url}
-            key = {movie.external_id}
-            />
-          </div>
-        );
-      });
-      setLibraryList(movieCollection);
-    })
-    .catch((error) => {
-      setError(error.response);
-    });
+  useEffect(() => {
+    axios.get('http://localhost:3000/movies')
+      .then((response) => {
+        const data = response.data;
+        console.log(`this is response.data ${response.data}`);
+        let movieCollection = data.map((movie) => {
+          return (
+            <div className="col-md-4 pb-4">
+              <MovieCard
+              external_id = {movie.external_id}
+              title = {movie.title}
+              overview = {movie.overview}
+              release_date = {movie.release_date}
+              image_url = {movie.image_url}
+              key = {movie.external_id}
+              />
+            </div>
+          );
+        });
+        setLibraryList(movieCollection);
+      })
+      .catch((error) => {
+        setError(error.response);
+      })
+  }, []);
 
     return (
       <div className='row m-5 pt-5'>{libraryList}</div>
