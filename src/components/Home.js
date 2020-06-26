e990f840c509295692dcc1e9e5f4947d98853c0c
mulhoo <oliviamulholland94@gmail.com>
2020-06-26 06:31:38 -0700

    updated homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import "./Home.css"
+import "./Home.css";
+import movienight from '../backgroundimages/ultimate-movie-weekend.jpg';
 
 const Home = () => {
   return (
     <div className="homepage">
+      <div>
+        <img className="homepage-photo" src={movienight} alt="movie-night"/>
+      </div>
       <nav>
         <div class="homepage-links">
           <p class="homepage-link">
