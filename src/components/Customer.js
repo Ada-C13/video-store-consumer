1f0747ef57882c6ba5086925fa3643983928fc87
peachmakkoli <lee.higgins.dev@gmail.com>
2020-06-25 22:36:29 -0700

    adds proptypes for customer

--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,13 +1,5 @@
-import React, { useEffect, useState, useCallback } from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  useRouteMatch,
-  useParams
-} from "react-router-dom";
-// import PropTypes from 'prop-types';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 // import './Customer.css';
 
@@ -26,6 +18,19 @@ const Customer = ({ id, name, registered_at, address, city, state, postal_code, 
       <p>Movies Checked Out: {movies_checked_out_count}</p>
     </section>
   );
+}
+
+Customer.propTypes = {
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  registered_at: PropTypes.string.isRequired,
+  address: PropTypes.string.isRequired,
+  city: PropTypes.string.isRequired,
+  state: PropTypes.string.isRequired,
+  postal_code: PropTypes.string.isRequired,
+  phone: PropTypes.string.isRequired,
+  account_credit: PropTypes.number.isRequired,
+  movies_checked_out_count: PropTypes.number.isRequired,
 }
 
 export default Customer;
